2fae2e9a83c8038437745ddcf42c4ba77f9dc60b
danielcolville <53836490+danielcolville@users.noreply.github.com>
2019-08-21 09:34:12 -0400

    Update 10_4.sql

--- 10_4.sql
+++ 10_4.sql
@@ -1,3 +1,6 @@
+select cname from `bank`.customer group by cname having (COUNT(*)>2) ;
+select * from customer group by cname;
+select * from customer order by cname;
 truncate customer;
 rename table account to customer_account;
 drop table customer;

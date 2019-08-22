6ec6f9fd7e20c9d1c83566efd0eaf976c991b7ea
danielcolville <53836490+danielcolville@users.noreply.github.com>
2019-08-22 10:36:52 -0400

    Add files via upload

---
+++ Assign 10_4.sql
@@ -0,0 +1,9 @@
+use bank;
+select cname from customer group by cname having (COUNT(*)>2) ;
+select * from customer group by cname;
+select * from customer order by cname;
+set foreign_key_checks=0;
+truncate customer;
+rename table account to customer_account;
+drop table customer;
+drop table customer_account;

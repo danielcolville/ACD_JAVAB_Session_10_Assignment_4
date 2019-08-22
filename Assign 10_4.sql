877fc3afbc30402ee8aeec1fc047573e53de809c
danielcolville <53836490+danielcolville@users.noreply.github.com>
2019-08-22 10:37:47 -0400

    Delete Assign 10_4.sql

--- Assign 10_4.sql
+++
@@ -1,9 +0,0 @@
-use bank;
-select cname from customer group by cname having (COUNT(*)>2) ;
-select * from customer group by cname;
-select * from customer order by cname;
-set foreign_key_checks=0;
-truncate customer;
-rename table account to customer_account;
-drop table customer;
-drop table customer_account;
